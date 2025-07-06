fc3ce2c406a55682b1a0c2e78efca0be3650f1f4
u <u>
2025-07-06 09:41:30 +0300

    update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import conf
 
@@ -12,6 +12,7 @@ attrs['name'] = attrs['pypi_name']
 
 
 setup(
+ packages=find_packages(),
  py_modules=['imp'],
  install_requires=[],
  **attrs
